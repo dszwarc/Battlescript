fb8ff36bdf789d81e518d7a21ef3487b26977649
dszwarc <dszwarc93@gmail.com>
2023-03-06 10:18:33 -0500

    added class constructor to make ship and function to generate ship div elements

--- js/app.js
+++ js/app.js
@@ -1,18 +1,45 @@
 //Declare Variables
 
+let size = 10;
+
+class Ship {
+    constructor(name, size){
+        this.name = name;
+        this.size = size;
+    }
+}
+
+    const destroyer = new Ship('destroyer', 3);
+    const submarine = new Ship('submarine', 3);
+    const battleship = new Ship('battleship',4);
+    const ships = [];
+    ships.push(destroyer, submarine, battleship);
+
+
+
+const shipEls = document.querySelectorAll('.ship');
+const gridSpaces = {
+    player: document.querySelectorAll('#p-board td'),
+    computer: document.querySelectorAll('#c-board td')
+}
+
+gridSpaces.player.addEventListener('mouseover', function(e){
+    e.target.style.backgroundColor = 'red'
+})
+
+
+
+
 
 
 //Dragging functionality
-const ships = document.querySelectorAll('.ship');
-//const gridSpaces = document.querySelectorAll('#c-board td')
-
-ships.forEach(ship =>{
+shipEls.forEach(ship =>{
     ship.addEventListener('dragstart', () => {
         ship.classList.add('dragging')
     })
 })
 
-ships.forEach(ship =>{
+shipEls.forEach(ship =>{
     ship.addEventListener('dragend',()=>{
         ship.classList.remove('dragging')
     })
@@ -28,7 +55,6 @@ ships.forEach(ship =>{
 //         grid.appendChild(ship)
 //     })
 // })
-
 //const click = document.querySelector('table').addEventListener('click', testclick)
 
 function testclick(evt){
@@ -36,7 +62,7 @@ function testclick(evt){
     evt.target.colorBackground = evt.target.colorBackground === 'rgb(75, 138, 201)' ? 'white' : 'rgb(75, 138, 201)';
 }
 
-
+init(size);
 
 //---------- Functions ------------//
 
@@ -44,12 +70,19 @@ function testclick(evt){
 //reappear dock, clear boards of hit/miss icons, replace turn indicator with "SET-UP PHASE"
 //
 
+function init(size){
+    generateTable('c-board', size);
+    generateTable('p-board', size);
+    generateShips();
+
+}
+
 //make all boats appear in dock
 //
 
 
 
-function fire(column , row){
+function fire(){
     //check to make sure it's player's turn, else skip to "AI" section
 
     //check location clicked on grid
@@ -68,17 +101,40 @@ function fire(column , row){
 
 function RandomGridLoc(){
     do {
-      const space = Math.floor(Math.random()*100);
+      const space = Math.floor(Math.random()*size*size);
       const isEmpty = document.querySelector(`#${space}`).classList;
     } while (isEmpty != 'empty');
     console.log(space);
     return space;
 }
 
-generateTable('c-board', 5);
-generateTable('p-board', 5);
 
 
+function generateShips(){
+    ships.forEach(ship =>{
+        const shipEl = document.createElement('div');
+        shipEl.draggable = true;
+        shipEl.classList.add('ship');
+        shipEl.id = ship.name;
+        document.querySelector('body').appendChild(shipEl);
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This function is good to go
 function generateTable(tablename, size){
     //list all possible column titles
     const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -86,12 +142,12 @@ function generateTable(tablename, size){
     const table = document.createElement('table');
     const tBody = document.createElement('tbody');
     table.id = tablename;
-    table.appendChild(tBody)
+    table.appendChild(tBody);
     //table.classList.add('board');
     
     //create first row with letters for looks
-    const letterRow = document.createElement('tr')
-    const blankSpace = document.createElement('th')
+    const letterRow = document.createElement('tr');
+    const blankSpace = document.createElement('th');
     blankSpace.textContent = '';
     letterRow.appendChild(blankSpace);
     for (let y = 0; y <= size-1; y++){
@@ -111,7 +167,7 @@ function generateTable(tablename, size){
         for (let u = 0; u < size; u++){
             let grid = document.createElement('td');
             grid.classList.add('empty');
-            grid.id = u;
+            grid.id = u + (i*size);
             row.appendChild(grid);
         }
         tBody.appendChild(row)
